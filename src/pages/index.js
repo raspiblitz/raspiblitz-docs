d58f274631747b24e7491cd8301aa58aa758af76
rootzoll <christian@geektank.de>
2024-10-07 18:26:59 +0200

    fix spellings

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <div style={{ padding: "20px 20px", fontSize: "85%", color: "lightgreen" }}>
-      <h2>RaspiBlitz - DIY Bitcoin &amp; Lightning Node on a RaspberryPi</h2>
-
+      <h2>RaspiBlitz - DIY Bitcoin & Lightning Node on a RaspberryPi</h2>
       <img
         src="https://github.com/rootzoll/raspiblitz/raw/dev/pictures/raspiblitz.jpg"
         alt="Raspberry Pi Blitz"
@@ -27,19 +26,18 @@ function HomepageHeader() {
           marginBottom: "6px",
         }}
       />
-      <div style={{ paddingTop: "15px" }}>
-        Download the lastest Version for Install & Update:
+      <div style={{ paddingTop: "10px" }}>
+        Download the latest Version for Install & Update:
         <ul style={{ padding: "0px 20px", listStyleType: "disc" }}>
           <li>
             <a href="https://docs.raspiblitz.org/docs/setup/software-setup/download">
-              RasperryPi SD-Card Images
+              RaspberryPi SD-Card Images
             </a>
           </li>
         </ul>
       </div>
-      <div>
-        <div style={{ paddingTop: "15px" }}>
-          Tutorial on how to build from Single Parts &amp; Setup:
+      <div style={{ paddingTop: "5px" }}>
+        Tutorial on how to build from Single Parts & Setup:
         <ul style={{ padding: "0px 20px", listStyleType: "disc" }}>
           <li>
             <a href="https://docs.raspiblitz.org/docs/setup/intro">
@@ -48,66 +46,48 @@ function HomepageHeader() {
           </li>
         </ul>
       </div>
-      <div>
+      <div style={{ paddingTop: "0px" }}>
         Source Code (MIT OpenSource):
         <ul style={{ padding: "0px 20px" }}>
           <li>
-            Main:{" "}
-            <a href="https://github.com/raspiblitz/raspiblitz">
-              github.com/raspiblitz/raspiblitz
-            </a>
+            Main: <a href="https://github.com/raspiblitz/raspiblitz">github.com/raspiblitz/raspiblitz</a>
           </li>
           <li>
-            WebUI:{" "}
-            <a href="https://github.com/raspiblitz/raspiblitz-web">
-              github.com/raspiblitz/raspiblitz-web
-            </a>
+            WebUI: <a href="https://github.com/raspiblitz/raspiblitz-web">github.com/raspiblitz/raspiblitz-web</a>
           </li>
           <li>
-            API:{" "}
-            <a href="https://github.com/fusion44/blitz_api">
-              github.com/fusion44/blitz_api
-            </a>
+            API: <a href="https://github.com/fusion44/blitz_api">github.com/fusion44/blitz_api</a>
           </li>
           <li>
-            Docs:{" "}
-            <a href="https://github.com/raspiblitz/raspiblitz-docs">
-              github.com/raspiblitz/raspiblitz-docs
-            </a>
-          </li>
-        </ul>
-        Questions and Support:
-        <ul style={{ padding: "0px 20px" }}>
-          <li>
-            <a href="https://t.me/raspiblitz">Telegram English</a> (community
-            managed)
-          </li>
-          <li>
-            <a href="https://t.me/raspiblitz_DE">Telegram Deutsch</a> (community
-            managed)
-          </li>
-          <li>
-            <a href="https://t.me/raspiblitz_ES">Telegram Spanish</a> (community
-            managed)
-          </li>
-          <li>
-            <a href="https://t.me/raspiblitz_IT">Telegram Italian</a> (community
-            managed)
-          </li>
-          <li>
-            <a href="https://t.me/raspiblitz_RU">Telegram Russian</a> (community
-            managed)
-          </li>
-          <li>
-            <a href="https://github.com/rootzoll/raspiblitz/issues?q=is%3Aissue">
-              GitHub Issues
-            </a>
+            Docs: <a href="https://github.com/raspiblitz/raspiblitz-docs">github.com/raspiblitz/raspiblitz-docs</a>
           </li>
         </ul>
       </div>
-
-      <div style={{ paddingTop: "15px" }}>
-        <a href="#shops">Shops that offer ready-made Hardware:</a>
+      <div style={{ paddingTop: "0px" }}>
+        Questions and Support:
+        <ul style={{ padding: "0px 20px" }}>
+          <li>
+            <a href="https://t.me/raspiblitz">Telegram English</a> (community managed)
+          </li>
+          <li>
+            <a href="https://t.me/raspiblitz_DE">Telegram Deutsch</a> (community managed)
+          </li>
+          <li>
+            <a href="https://t.me/raspiblitz_ES">Telegram Spanish</a> (community managed)
+          </li>
+          <li>
+            <a href="https://t.me/raspiblitz_IT">Telegram Italian</a> (community managed)
+          </li>
+          <li>
+            <a href="https://t.me/raspiblitz_RU">Telegram Russian</a> (community managed)
+          </li>
+          <li>
+            <a href="https://github.com/rootzoll/raspiblitz/issues?q=is%3Aissue">GitHub Issues</a>
+          </li>
+        </ul>
+      </div>
+      <div style={{ paddingTop: "0px" }}>
+        Shops that offer ready-made Hardware:
         <ul style={{ padding: "0px 20px" }}>
           <li>
             <a href="https://diynodes.com">DIYnodes.com</a>
@@ -117,10 +97,10 @@ function HomepageHeader() {
           </li>
         </ul>
       </div>
-
-      <div style={{ paddingTop: "15px" }}>
-        <a href="#donation">Scan or Click to Donate to the RaspiBlitz Project:</a>
-        <img
+      <div style={{ paddingTop: "0px" }}>
+        <a href="#donation" />
+        Scan or Click to Donate to the RaspiBlitz Project:<br />
+        <a href="https://yourdevice.ch/produkt-kategorie/btc-nodes-zubehoer/"><img
           src="https://github.com/rootzoll/raspiblitz/raw/dev/pictures/qr_donate.png"
           alt="Donate to RaspiBlitz Project with Lightning Address"
           style={{
@@ -130,17 +110,11 @@ function HomepageHeader() {
             marginTop: "10px",
             marginBottom: "6px",
           }}
-        />
-        Lightning Address: raspiblitz@btcypay.fulmo.org<br />All donations for the RaspiBlitz project are collected by{" "}
-        <a href="https://fulmo.org" target="_blank">
-          Fulmo
-        </a>{" "}
-        &amp; managed together with the developer team.
+        /></a><br />
+        Lightning Address: raspiblitz@btcypay.fulmo.org<br />All donations for the RaspiBlitz project are collected by <a href="https://fulmo.org" target="_blank">Fulmo</a> & managed together with the developer team.
       </div>
-
-      <div style={{ paddingTop: "15px" }}>
-        Impressum: Christian Rotzoll, christian [ at ] rotzoll.de - this site is
-        not using any cookies and is not storing your data.
+      <div style={{ paddingTop: "10px" }}>
+        Impressum: Christian Rotzoll, christian [ at ] rotzoll.de - this site is not using any cookies and is not storing your data.
       </div>
     </div>
   );
